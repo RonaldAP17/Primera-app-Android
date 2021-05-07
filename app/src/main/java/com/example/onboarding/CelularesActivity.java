10d5e4eea39c5025f1de010e05c2b7fc3ccdade8
RonaldAP17 <ronaldocris324@gmail.com>
2021-05-07 03:22:50 -0500

    Cambio N°22: Los activities de la sección de celulares son completamente funcionales. *Se espera añadir los fragments correspondientes.

--- app/src/main/java/com/example/onboarding/CelularesActivity.java
+++ app/src/main/java/com/example/onboarding/CelularesActivity.java
@@ -9,18 +9,36 @@ import android.widget.ImageButton;
 
 public class CelularesActivity extends AppCompatActivity {
 
-    ImageButton mImgBtnXiaomi;
+    ImageButton mImgBtnXiaomi,mImgBtnApple,mImgBtnNotion ,mImgBtnToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_celulares);
         mImgBtnXiaomi = findViewById(R.id.imgbtnXiaomi);
+        mImgBtnApple = findViewById(R.id.imgbtnApple);
+        mImgBtnNotion = findViewById(R.id.imgbtnNotion);
+        mImgBtnToDo = findViewById(R.id.imgbtnToDo);
 
-        Intent intent = new Intent(this,XiaomiActivity.class);
+        Intent intent1 = new Intent(this,XiaomiActivity.class);
+        Intent intent2 = new Intent(this,AppleActivity.class);
+        Intent intent3 = new Intent(this,NotionActivity.class);
+        Intent intent4 = new Intent(this,ToDoActivity.class);
     }
 
     public void verPaginaXiaomi(View view) {
             startActivity(new Intent(this, XiaomiActivity.class));
         }
+
+    public void verPaginaApple(View view) {
+        startActivity(new Intent(this, AppleActivity.class));
     }
+
+    public void verPaginaNotion(View view) {
+        startActivity(new Intent(this, NotionActivity.class));
+    }
+
+    public void verPaginaToDo(View view) {
+        startActivity(new Intent(this, ToDoActivity.class));
+    }
+}
