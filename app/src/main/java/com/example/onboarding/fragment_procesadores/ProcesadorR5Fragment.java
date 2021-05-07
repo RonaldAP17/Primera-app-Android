8f6d650ee04dc05c7152845d4a5a1c2243991d00
Redtubecrash <isilgrupo2@gmail.com>
2021-05-07 00:52:46 -0500

    Cambio N°15: -Se agregaron los fragments restantes de los productos
    *Falta terminar de unirlos con sus botones y tambien falta terminar el menú.
                                             Cambio y Fuera

---
+++ app/src/main/java/com/example/onboarding/fragment_procesadores/ProcesadorR5Fragment.java
@@ -0,0 +1,66 @@
+package com.example.onboarding.fragment_procesadores;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.onboarding.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProcesadorR5Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProcesadorR5Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProcesadorR5Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProcesadorR5Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProcesadorR5Fragment newInstance(String param1, String param2) {
+        ProcesadorR5Fragment fragment = new ProcesadorR5Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_procesador_r5, container, false);
+    }
+}
