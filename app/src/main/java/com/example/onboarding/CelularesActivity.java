42c24cb66d6c08968674ff76d704b6331874d714
RonaldAP17 <ronaldocris324@gmail.com>
2021-05-06 23:51:54 -0500

    Cambio N°14: Cambio estetico al primer fragment de los monitores. -Se establecio el diseño anterior de los textos. *Se espera aplicar esto con todas las secciones

--- app/src/main/java/com/example/onboarding/CelularesActivity.java
+++ app/src/main/java/com/example/onboarding/CelularesActivity.java
@@ -3,7 +3,6 @@ package com.example.onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 public class CelularesActivity extends AppCompatActivity {
 
@@ -11,11 +10,5 @@ public class CelularesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_celulares);
-    }
-
-    public void verPaginaXiaomi(View view) {
-    }
-
-    public void verPaginaApple(View view) {
     }
 }
